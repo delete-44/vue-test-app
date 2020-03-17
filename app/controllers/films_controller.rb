c4f9a7e84679f1ddfde7ea23c6ed64074d34d7ac
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2020-03-17 19:30:13 +0000

    Data successfully updating

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -1,5 +1,21 @@
 class FilmsController < ApplicationController
+  before_action :fetch_film, except: :index
+
   def index
-    @films = Film.all
+    @films = Film.all.order(:id)
+  end
+
+  def update
+    @film.update(film_params)
+  end
+
+  private
+
+  def fetch_film
+    @film = Film.find(params[:id])
+  end
+
+  def film_params
+    params.require(:film).permit(:id, :title, :description)
   end
 end
--- app/javascript/films-table.vue
+++ app/javascript/films-table.vue
@@ -4,12 +4,19 @@
       :items='items'
       :fields='fields'
       >
+      <template v-slot:cell(title)='row'>
+        <b-form-input v-model='row.item.title' v-on:change.native="dataChanged"/>
+      </template>
+
+      <template v-slot:cell(description)='row'>
+        <b-form-input v-model='row.item.description' v-on:change.native="dataChanged"/>
+      </template>
     </b-table>
   </div>
 </template>
 
 <script>
-  import axios from 'axios'
+  import axios from 'axios-on-rails'
 
   export default {
     data() {
@@ -22,6 +29,21 @@
       axios
       .get('/films.json')
       .then(response => (this.items = response.data))
+    },
+    methods: {
+      dataChanged(e) {
+        let row = e.target.closest('tr')
+        let film = this.items[row.rowIndex - 1]
+
+        axios
+          .put('/films/' + film.id,
+          {
+            film: {
+              title: row.children[1].children[0]._value,
+              description: row.children[2].children[0]._value
+            }
+          })
+      },
     }
   }
 </script>
