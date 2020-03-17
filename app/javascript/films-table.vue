74124cdfe5b5122360524a833ffd6c6272d74efd
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2020-03-17 17:14:48 +0000

    Added static table

---
+++ app/javascript/films-table.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <b-table striped hover
+      :items='items'
+      :fields='fields'
+      >
+    </b-table>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        fields: [{ ID: 'id', Title: 'title', Description: 'description' }],
+        items: [
+          {
+            id: 0,
+            title: 'My Neighbour Totoro',
+            description: 'Mei and Satsuki shift to a new house to be closer to their mother...',
+          },
+          {
+            id: 1,
+            title: 'Spirited Away',
+            description: 'Chihiro and her parents stumble upon a seemingly abandoned amusement park...'
+          },
+        ]
+      };
+    },
+  }
+</script>
+
+
+<style scoped>
+  body { padding: 1rem; }
+</style>
--- app/javascript/packs/films-list.js
+++ app/javascript/packs/films-list.js
@@ -1,9 +1,22 @@
 // app/javascript/packs/films-list.js
 import Vue from 'vue'
 import List from '../films-list.vue'
+import Table from '../films-table.vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import BootstrapVue from 'bootstrap-vue'
+Vue.use(BootstrapVue)
+
 document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: '#filmList',
     render: h => h(List),
   });
+
+  new Vue({
+    el: '#filmTable',
+    render: h => h(Table),
+  })
 });
