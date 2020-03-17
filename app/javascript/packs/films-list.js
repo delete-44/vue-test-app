593457b1a190d393c0e86ef1c03797e44fd7f3a9
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2020-03-17 18:06:29 +0000

    Using bootstrap sitewide

--- app/javascript/packs/films-list.js
+++ app/javascript/packs/films-list.js
@@ -1,10 +1,9 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 import Table from '../films-table.vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-// And import the bootstrap vue package to use
-import BootstrapVue from 'bootstrap-vue'
 Vue.use(BootstrapVue)
 
 document.addEventListener('DOMContentLoaded', () => {
